a78218b754f807b55dbf9c04eab66cdd4460d459
franckmassa <massafranck@gmail.com>
2020-07-20 18:45:05 +0200

    Paginantion avec le bundle KnpPaginator Guithub

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 use App\Repository\VoitureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoitureController extends AbstractController
 {
     /**
      * @Route("/client/voitures", name="voitures")
      */
-    public function index(VoitureRepository $repo)
+    public function index(VoitureRepository $repo, PaginatorInterface $paginatorInterface, Request $request)
     {
-        $voitures = $repo->findAll();
+        $voitures = $paginatorInterface->paginate(
+            $repo->findAllWithPagination(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            6 /*limit per page*/
+    );
         return $this->render('voiture/voitures.html.twig', [
             "voitures" => $voitures
         ]);
