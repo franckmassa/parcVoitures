1dcf5078323c33e3d53a6dc33166c55739d604ce
franckmassa <massafranck@gmail.com>
2020-07-21 22:48:35 +0200

    formulaire d'inscription avec mot de passe protégé

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class GlobalController extends AbstractController
 {
@@ -13,6 +18,31 @@ class GlobalController extends AbstractController
     public function index()
     {
         return $this->render('global/accueil.html.twig');
+            
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $EntityManager, UserPasswordEncoderInterface $encoder)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $passwordCrypte = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $utilisateur->setRoles('ROLE_USER');
+            $EntityManager->persist($utilisateur);
+            $EntityManager->flush();
+            // $this->$addFlash('success', "Vous êtes bien inscrit");
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('global/inscription.html.twig', [
+            "form" => $form->createView(),
+        ]);
             
     }
 }
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UtilisateurRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @UniqueEntity(
+ * fields = {"username"},
+ * message="L'utilisateur existe déjà"
+ * )
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Les mot de passe sont différents")
+    */
+    private $verifPassword;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getVerifPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setVerifPassword(string $verifPassword): self
+    {
+        $this->verifPassword = $verifPassword;
+
+        return $this;
+    }
+
+    public function getRoles(): ?string
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(string $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        
+    }
+
+    public function getSalt()
+    {
+        
+    }
+}
